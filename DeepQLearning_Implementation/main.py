d7b184fd5491af7e2e351ea326c3a02cf1d4cc71
DACUS1995 <studormarian@gmail.com>
2020-02-21 20:03:18 +0200

    xxx

--- DeepQLearning_Implementation/main.py
+++ DeepQLearning_Implementation/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import argparse
 import gym
 import random
+from matplotlib import pyplot as plt
 
 from model import FrozenLakeModel
 from memory import MemoryReplay
@@ -30,9 +31,9 @@ def train(
 	exploration_rate = 0.1
 	replay_memory = MemoryReplay(memory_size)
 
-	target_netowrk = FrozenLakeModel()
+	target_network = FrozenLakeModel()
 	policy_network = FrozenLakeModel()
-	target_netowrk.to(device)
+	target_network.to(device)
 	policy_network.to(device)
 
 	policy_network.train()
@@ -40,7 +41,9 @@ def train(
 	optimizer = torch.optim.Adam(policy_network.parameters())
 	criterion = nn.MSELoss()
 	count = 1000
-	count_2 = 100
+
+	loss_all = []
+	reward_all = []
 
 	running_loss = 0
 
@@ -48,9 +51,12 @@ def train(
 
 	#Training loop
 	for ep in range(num_episodes):
-		# print(f'---> Running episode [{ep}/{num_episodes}]')
+		rewards_ep = 0
+
+		if ep % 50 == 0:
+			print(f'---> Running episode [{ep}/{num_episodes}]')
+
 		state = torch.tensor([env.reset()], device=device, dtype=torch.float)
-		rewards_ = 0
 
 		for step in range(max_steps_per_ep):
 			random_threshold = random.uniform(0, 1)
@@ -64,14 +70,14 @@ def train(
 
 
 			new_state, reward, done, _ = env.step(action)
-			rewards_ += reward
-			print(action)
-			print(done)
+			rewards_ep += reward
 
 			reward = torch.tensor(reward, device=device)
 			new_state = torch.tensor([new_state], device=device, dtype=torch.float)
 			action = torch.tensor(action, device=device, dtype=torch.float)
 			replay_memory.add_sample((state, action, reward, new_state))
+
+			state = new_state
 
 			# Optimize the model
 			if len(replay_memory) >= batch_size:
@@ -89,9 +95,9 @@ def train(
 				new_states = new_states.reshape((-1, 1))
 
 				policy_next_action = policy_network(states).gather(1, actions).squeeze()
-				target_next_action = target_netowrk(new_states).detach()
+				target_next_next_action = target_network(new_states).detach()
 
-				target_expected_next_action = rewards + Config.DISCOUNT * target_next_action.max(1)[0]
+				target_expected_next_action = rewards + Config.DISCOUNT * target_next_next_action.max(1)[0]
 
 				loss = criterion(policy_next_action, target_expected_next_action)
 				running_loss += loss.item()
@@ -103,22 +109,36 @@ def train(
 
 			if done:
 				break
-			state = new_state
 
 		# print(running_loss / max_steps_per_ep)
+
+		loss_all.append(running_loss)
+		reward_all.append(rewards_ep)
+
 		running_loss = 0
+		rewards_ep = 0
 
 		# Update the target network
-		if ep % 10 == 0:
-			target_netowrk.load_state_dict(policy_network.state_dict())
+		if ep % 5 == 0:
+			target_network.load_state_dict(policy_network.state_dict())
 
 		# Training metrics
-		if (ep + 1) % 1000 == 0:
-			print(f'{count} : {rewards_ / 1000}')
-			rewards_ = 0
+		# if (ep + 1) % 1000 == 0:
+		# 	print(f'{count} : {rewards_ep / 1000}')
+		# 	rewards_ep = 0
 
 		# Lower the exploration rate
-		exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * ep)
+		# exploration_rate = min_exploration_rate + (max_exploration_rate - min_exploration_rate) * np.exp(-exploration_decay_rate * ep)
+
+	plt.subplot(2, 1, 1)
+	plt.ylabel("Loss")
+	plt.plot(loss_all)
+
+	plt.subplot(2, 1, 2)
+	plt.plot(reward_all)
+	plt.xlabel("Episodes")
+	plt.ylabel("Reward")
+	plt.show()
 
 def main(args):
 	train(
@@ -132,9 +152,9 @@ def main(args):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser("")
 	parser.add_argument("-l", "--learning-rate", type=float, default=0.05, help="learning rate")
-	parser.add_argument("-ep", "--episodes", type=int, default=10000, help="number of episodes")
+	parser.add_argument("-ep", "--episodes", type=int, default=5000, help="number of episodes")
 	parser.add_argument("-mem", "--replay-memory-size", type=int, default=1000, help="replay memory size")
-	parser.add_argument("--max-steps-per-ep", type=int, default=100, help="max steps for episode")
-	parser.add_argument("--batch-size", type=int, default=5, help="model optimization batch sizes")
+	parser.add_argument("--max-steps-per-ep", type=int, default=1000, help="max steps for episode")
+	parser.add_argument("--batch-size", type=int, default=32, help="model optimization batch sizes")
 	args = parser.parse_args()
 	main(args)
--- DeepQLearning_Implementation/memory.py
+++ DeepQLearning_Implementation/memory.py
@@ -1,11 +1,12 @@
 import numpy as np
 import random
 from typing import List, Tuple
+from collections import deque
 
 class MemoryReplay():
     def __init__(self, size: int) -> None:
         self._size = size
-        self._memory = []
+        self._memory = deque(maxlen=self._size)
 
     def get_sample(self, batch_size=1) -> List[Tuple]:
         if len(self._memory) < batch_size:
@@ -15,9 +16,6 @@ class MemoryReplay():
 
 
     def add_sample(self, sample: Tuple) -> None:
-        if len(self._memory) == self._size:
-            self._memory.pop(0)
-        
         self._memory.append(sample)
 
     def __len__(self) -> int:
